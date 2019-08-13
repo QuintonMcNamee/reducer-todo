60861102968a0053ecc622c855fec1290bc903b3
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-13 18:25:34 -0400

    nonsense

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -4,16 +4,54 @@ import React, { useState, useReducer } from 'react';
 import { initialState, todoListReducer } from '../reducers/todoListReducer';
 
 const TodoList = () => {
-  const [newTodoListText, setNewTodoListText] = useState();
+  const [newItemText, setNewItemText] = useState();
 
   const [state, dispatch] = useReducer(todoListReducer, initialState);
   console.log(state);
 
+  const handleChanges = e => {
+    setNewItemText(e.target.value);
+  };
+
   return (
+  <div>
+
+
     <div>
-      {state.map(item => <div>{item.item}</div>)}
+      {!state.editing ? (
+        <h1>
+          {state.todos.map(item => (
+            <>
+            <div>
+              {item.item}
+            </div>
+        <button
+          onClick={() => dispatch({ type: 'TOGGLE_EDITING' })}
+        >new item</button>
+        </>
+        ))}
+      </h1>
+    ) : (
+      <div>
+        <input
+          className="item-input"
+          type="text"
+          name="newItemText"
+          value={newItemText}
+          onChange={handleChanges}
+        />
+        <button
+          onClick={() =>
+            dispatch({ type: 'UPDATE_ITEM', payload: newItemText })
+          }
+        >
+          Update Item
+        </button>
+      </div>
+    )}
     </div>
-  )
-}
+  </div>
+  );
+};
 
 export default TodoList;
--- my-app/src/reducers/todoListReducer.js
+++ my-app/src/reducers/todoListReducer.js
@@ -1,21 +1,47 @@
-export const initialState = [
-  {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-  },
-  {
-    item: 'Do reducers project',
-    completed: false,
-    id: 3892987590
-  },
-  {
-    item: 'Celebrate',
-    completed: false,
-    id: 3892987591
-  }
-]
-
-export const todoListReducer = () => {
-  return 0;
+export const initialState = {
+  todos:[
+    {
+      item: 'Learn about reducers',
+      completed: false,
+      editing: false,
+      id: 3892987589
+    },
+    {
+      item: 'Do reducers project',
+      completed: false,
+      editing: false,
+      id: 3892987590
+    },
+    {
+      item: 'Celebrate',
+      completed: false,
+      editing: false,
+      id: 3892987591
+    }
+  ],
+  newTodo:
+    {
+      item: '',
+      completed: false,
+      editing: false,
+      id: new Date()
+    }
 }
+
+export const todoListReducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_EDITING':
+      return {
+        ...state,
+        editing: !state.todos.editing
+      };
+    case 'UPDATE_ITEM':
+      return {
+        ...state,
+        item: action.payload,
+        editing: !state.todos.editing
+      };
+    default:
+      return state;
+  }
+};
