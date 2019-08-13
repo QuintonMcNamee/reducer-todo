2c82a3a1331e45f3b7bc4b55b7eaf098f77caca9
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-13 16:25:41 -0400

    step 1 complete

---
+++ my-app/src/reducers/reducer.js
@@ -0,0 +1,13 @@
+import React, { useReducer } from 'react'
+
+const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+}
+
+const reducer = () => {
+  return default;
+}
+
+export default (initialState, reducer);
